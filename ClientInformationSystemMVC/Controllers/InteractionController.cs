49c9c90e18fe6281b2473b687db783743d7877f1
sarahafg <sfakhry24@gmail.com>
2021-11-19 00:16:04 -0500

    ClientInformationSystem Pretty

---
+++ ClientInformationSystemMVC/Controllers/InteractionController.cs
@@ -0,0 +1,43 @@
+using ApplicationCore.ServiceInterfaces;
+using ClientInformationSystemMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientInformationSystemMVC.Controllers
+{
+    public class InteractionController : Controller
+    {
+        private readonly IDataService _dataService;
+
+        public InteractionController(IDataService dataService)
+        {
+            _dataService = dataService;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var interCards = await _dataService.GetAllInteraction();
+            if (!interCards.Any())
+            {
+                return View();
+            }
+            return View(interCards);
+            //return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
